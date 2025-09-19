3bcafad3831e38e6ea59cab25e70c17b54fa346e
IKE118\ikeyu <ikeyuradesu118@ezweb.ne.jp>
2025-09-19 23:01:45 +0900

    名前と数字を自分で入力するようにした

--- BaseTest.java
+++ BaseTest.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class BaseTest {
     public static void main(String[] args) {
         int num1 = 10;
-        Scanner sc1 = new Scanner(System.in);
-        System.out.println(num1 + "に足す値を入力してください");
-        int num2 = sc1.nextInt();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("あなたの名前を教えてください");
+        String name = sc.nextLine();
+
+        System.out.println("こんにちは " + name + " さん! " + num1 + " に足す値を入力してください");
+        int num2 = sc.nextInt();
+
         int result = num1 + num2;
-        System.out.println("num + num2 = " + result + "です");
-        sc1.close();
+        System.out.println("num1 + num2 = " + result + " です");
+
+        sc.close();
     }
 }
