e98e88dc5ea6688f2a080ebf8bed17d806fcb34f
IKE118\ikeyu <ikeyuradesu118@ezweb.ne.jp>
2025-10-08 21:39:25 +0900

    関数を入れて数字当て実装

--- BaseTest.java
+++ BaseTest.java
@@ -2,14 +2,16 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class BaseTest {
+
+    private static final int WIDE_DISTANCE = 50;
+    private static final int SMALL_DISTANCE = 20;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m_Answer = 0;
         boolean m_Check = false;
         Random rand = new Random();
 
-        final int WIDE_DISTANCE = 50;
-        final int SMALL_DISTANCE = 20;
         // 0~100までの乱数を入れる
         int m_RandNum = rand.nextInt(101);
         int m_PlayCount = 0;
@@ -23,23 +25,9 @@ public class BaseTest {
             if (l_Distance == 0) {
                 m_Check = true;
             } else {
-                if (m_Answer < m_RandNum) {
-                    if (l_Distance < SMALL_DISTANCE) {
-                        System.out.println("答えの数字はもう少し大きいよ!");
-                    } else if (l_Distance < WIDE_DISTANCE) {
-                        System.out.println("答えの数字はまだ大きいよ!");
-                    } else {
-                        System.out.println("答えの数字は全然大きいよ!");
-                    }
-                } else {
-                    if (l_Distance < SMALL_DISTANCE) {
-                        System.out.println("答えの数字はもう少し小さいよ!");
-                    } else if (l_Distance < WIDE_DISTANCE) {
-                        System.out.println("答えの数字はまだ小さいよ!");
-                    } else {
-                        System.out.println("答えの数字は全然小さいよ!");
-                    }
-                }
+                // ヒントを与える
+                boolean l_IsLow = (m_Answer < m_RandNum);
+                System.out.println(HitMassage(l_Distance, l_IsLow));
             }
             // 5回目からはヒントをだす
             if (m_PlayCount > 4) {
@@ -49,5 +37,16 @@ public class BaseTest {
         // プレイ回数を出す
         System.out.println("お疲れ様でした!プレイ回数は" + m_PlayCount + "です!");
         sc.close();
+    }
+
+    static private String HitMassage(int distance, boolean isLow) {
+        String dir = isLow ? "大きい" : "小さい"; // 回答の大きさで決める
+        if (distance < SMALL_DISTANCE) {
+            return "答えの数字は、もう少し" + dir + "よ!";
+        } else if (distance < WIDE_DISTANCE) {
+            return "答えの数字は、まだ" + dir + "よ!";
+        } else {
+            return "答えの数字は、全然" + dir + "よ!";
+        }
     }
 }
