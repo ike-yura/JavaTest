1cf177971a3c8d20667a6e66fe7506c4878ff814
IKE118\ikeyu <ikeyuradesu118@ezweb.ne.jp>
2025-10-08 22:29:40 +0900

    じゃんけんプログラムの実装をしている

--- BaseTest.java
+++ BaseTest.java
@@ -2,51 +2,57 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class BaseTest {
-
-    private static final int WIDE_DISTANCE = 50;
-    private static final int SMALL_DISTANCE = 20;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m_Answer = 0;
-        boolean m_Check = false;
         Random rand = new Random();
 
-        // 0~100までの乱数を入れる
-        int m_RandNum = rand.nextInt(101);
-        int m_PlayCount = 0;
-        // 数字当てるまでは無限ループ
-        while (!m_Check) {
-            System.out.println("こんにちは! 0 ~ 100の値を入力してください!");
-            m_Answer = sc.nextInt();
-            int l_Distance = Math.abs(m_RandNum - m_Answer);
-            m_PlayCount++;
-            // 数字が当たっていれば終わり
-            if (l_Distance == 0) {
-                m_Check = true;
+        int m_EnemyHand = 0;
+        int m_PlayerHand = 0;
+        boolean m_Finish = false;
+        System.out.println("じゃんけんをはじめます!じゃーんけーんぽい!");
+
+        while (!m_Finish) {
+            System.out.println("出す手を決めてください,0ならグー,1ならチョキ,2ならパー");
+            // 0~2までの乱数を入れる(0 = グー: 1 = チョキ: 2 = パー )
+            m_EnemyHand = rand.nextInt(3);
+            // データの整数チェック
+            if (sc.hasNextInt()) {
+                m_PlayerHand = sc.nextInt();// プレイヤーの出した手
+                System.out.println(
+                        "プレイヤーが出した手は" + HandMassege(m_PlayerHand) + "です。敵が出した手は" + HandMassege(m_EnemyHand) + "です。");
+                if (m_PlayerHand >= 0 && m_PlayerHand <= 2) {
+                    if (m_EnemyHand == m_PlayerHand) {
+                        System.out.println("あいこです。もう一回やりましょう");
+                        continue;
+                    } else if ((m_PlayerHand == 0 && m_EnemyHand == 1) || (m_PlayerHand == 1 && m_EnemyHand == 2)
+                            || (m_PlayerHand == 2 && m_EnemyHand == 0)) {
+                        System.out.println("あなたの勝ちです！");
+                        m_Finish = true;
+                    } else {
+                        System.out.println("あなたの負けです・・・");
+                        m_Finish = true;
+                    }
+                } else {
+                    System.out.println("範囲外の数字です");
+                }
             } else {
-                // ヒントを与える
-                boolean l_IsLow = (m_Answer < m_RandNum);
-                System.out.println(HitMassage(l_Distance, l_IsLow));
-            }
-            // 5回目からはヒントをだす
-            if (m_PlayCount > 4) {
-                System.out.println("ヒントです!答えとの差は" + l_Distance + "です!");
+                System.out.println("整数ではないのでエラーです");
             }
         }
-        // プレイ回数を出す
-        System.out.println("お疲れ様でした!プレイ回数は" + m_PlayCount + "です!");
         sc.close();
     }
 
-    static private String HitMassage(int distance, boolean isLow) {
-        String dir = isLow ? "大きい" : "小さい"; // 回答の大きさで決める
-        if (distance < SMALL_DISTANCE) {
-            return "答えの数字は、もう少し" + dir + "よ!";
-        } else if (distance < WIDE_DISTANCE) {
-            return "答えの数字は、まだ" + dir + "よ!";
+    static private String HandMassege(int Hand) {
+        String l_Massage = "";
+        // じゃんけんの手
+        if (Hand == 0) {
+            l_Massage = "グー";
+        } else if (Hand == 1) {
+            l_Massage = "チョキ";
         } else {
-            return "答えの数字は、全然" + dir + "よ!";
+            l_Massage = "パー";
         }
+
+        return l_Massage;
     }
 }
